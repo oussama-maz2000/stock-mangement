c54ee95737d540b32ec714b1e054bfc69390d856
OMZ_IT <loulououssama2015@gmail.com>
2026-04-20 20:44:05 +0100

    User service and User service imp

---
+++ stock-management/stock-management/src/main/java/com/stock/stock_management/services/implementations/UserServiceImp.java
@@ -0,0 +1,64 @@
+package com.stock.stock_management.services.implementations;
+
+
+import com.stock.stock_management.dtos.user.AddUserDto;
+import com.stock.stock_management.dtos.user.UpdateUserDto;
+import com.stock.stock_management.dtos.user.UserDto;
+import com.stock.stock_management.models.User;
+import com.stock.stock_management.services.interfaces.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImp implements UserService, UserDetailsService {
+    @Override
+    public void createUser(AddUserDto addUserDto) {
+
+    }
+
+    @Override
+    public User findUserById(String id) {
+        return null;
+    }
+
+    @Override
+    public Page<UserDto> findAllUsers(String fullName, String username, String email, String phone, String roleId, Boolean enabled, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public void updateUser(String id, UpdateUserDto updateUserDto) {
+
+    }
+
+    @Override
+    public void enableUser(String id) {
+
+    }
+
+    @Override
+    public void disableUser(String id) {
+
+    }
+
+    @Override
+    public void deleteUser(String id) {
+
+    }
+
+    @Override
+    public void updateUserRole(String userId, String roleId) {
+
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
---
+++ stock-management/stock-management/src/main/java/com/stock/stock_management/services/interfaces/UserService.java
@@ -0,0 +1,24 @@
+package com.stock.stock_management.services.interfaces;
+
+import com.stock.stock_management.dtos.user.AddUserDto;
+import com.stock.stock_management.dtos.user.UpdateUserDto;
+import com.stock.stock_management.dtos.user.UserDto;
+import com.stock.stock_management.models.User;
+import org.madaure.backoffice.dtos.user.AddUserDto;
+import org.madaure.backoffice.dtos.user.UpdateUserDto;
+import org.madaure.backoffice.dtos.user.UserDto;
+import org.madaure.backoffice.models.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService{
+    void createUser(AddUserDto addUserDto);
+    User findUserById(String id);
+    Page<UserDto> findAllUsers(String fullName,String username,String email,String phone,String roleId,Boolean enabled,Pageable pageable);
+    void updateUser(String id, UpdateUserDto updateUserDto);
+    void enableUser(String id);
+    void disableUser(String id);
+    void deleteUser(String id);
+    void updateUserRole(String userId,String roleId);
+}
