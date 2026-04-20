708ce78348167d46a632925798aedd21d4c45266
OMZ_IT <loulououssama2015@gmail.com>
2026-04-20 20:44:35 +0100

    add new fun of user mapper

--- stock-management/stock-management/src/main/java/com/stock/stock_management/utils/OwnMapper.java
+++ stock-management/stock-management/src/main/java/com/stock/stock_management/utils/OwnMapper.java
@@ -3,7 +3,11 @@ package com.stock.stock_management.utils;
 
 import com.stock.stock_management.dtos.role.AddRoleDto;
 import com.stock.stock_management.dtos.role.RoleDto;
+import com.stock.stock_management.dtos.user.AddUserDto;
+import com.stock.stock_management.dtos.user.UpdateUserDto;
+import com.stock.stock_management.dtos.user.UserDto;
 import com.stock.stock_management.models.Role;
+import com.stock.stock_management.models.User;
 import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
 
@@ -16,4 +20,10 @@ public interface OwnMapper {
     Role toRole(AddRoleDto addRoleDto);
     RoleDto toRoleDto(Role role);
 
-}
+
+
+    /* USER MAPPERS */
+    User toUser(AddUserDto addUserDto);
+    @Mapping(target = "role", expression = "java(user.getRole() != null ? user.getRole().getName() : null)")
+    UserDto toUserDto(User user);
+    void mapUserFromDto(UpdateUserDto userDto, @MappingTarget User user);}
