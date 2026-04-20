ee650da665df45b35816bbcdb2baae71f483cf0b
OMZ_IT <loulououssama2015@gmail.com>
2026-04-20 20:44:52 +0100

    disable flyway

--- stock-management/stock-management/src/main/resources/db/migration/V2__init_role_user.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO roles (created_by,name ) VALUES (NULL,'ROLE_SUPER_ADMIN');
-INSERT INTO users (role_id,username, email, password)
-VALUES (1,'oussama', 'oussama@stock.com', '$2a$12$eEQ0QWyxCWuO9UAlSKpR7.yn3PhpUW0la5MdRfwM5KNKHzZKzLBIW' );
