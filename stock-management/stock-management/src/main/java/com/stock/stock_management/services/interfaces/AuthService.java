c546e7f39f11a8296154085eb0a42ab89fd9c28e
OMZ_IT <loulououssama2015@gmail.com>
2026-04-21 15:35:24 +0100

    added HttpServletResponse  as param

--- stock-management/stock-management/src/main/java/com/stock/stock_management/services/interfaces/AuthService.java
+++ stock-management/stock-management/src/main/java/com/stock/stock_management/services/interfaces/AuthService.java
@@ -2,9 +2,10 @@ package com.stock.stock_management.services.interfaces;
 
 import com.stock.stock_management.dtos.auth.LoginRequest;
 import com.stock.stock_management.dtos.auth.LoginResponse;
+import jakarta.servlet.http.HttpServletResponse;
 
 public interface AuthService {
 
-    LoginResponse login(LoginRequest login);
+    LoginResponse login(LoginRequest login,HttpServletResponse httpServletResponse);
 
 }
