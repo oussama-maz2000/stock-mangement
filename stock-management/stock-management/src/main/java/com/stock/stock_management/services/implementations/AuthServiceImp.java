3f7b7a8b1ac0cab0c382fdce08070a13838388c5
OMZ_IT <loulououssama2015@gmail.com>
2026-04-21 15:34:26 +0100

    added HttpReponseServlet as param

--- stock-management/stock-management/src/main/java/com/stock/stock_management/services/implementations/AuthServiceImp.java
+++ stock-management/stock-management/src/main/java/com/stock/stock_management/services/implementations/AuthServiceImp.java
@@ -7,8 +7,10 @@ import com.stock.stock_management.exceptions.DeletedUserException;
 import com.stock.stock_management.exceptions.InvalidAuthenticationException;
 import com.stock.stock_management.models.User;
 import com.stock.stock_management.services.interfaces.AuthService;
+import com.stock.stock_management.services.interfaces.CookieService;
 import com.stock.stock_management.utils.JwtUtils;
 import com.stock.stock_management.utils.OwnMapper;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.*;
 import org.springframework.security.core.Authentication;
@@ -21,14 +23,18 @@ public class AuthServiceImp implements AuthService {
 
     private final AuthenticationManager authenticationManager;
     private final JwtUtils jwtUtils;
+    private final CookieService cookieService;
     @Override
-    public LoginResponse login(LoginRequest login) {
+    public LoginResponse login(LoginRequest login, HttpServletResponse httpServletResponse) {
         try {
             Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(login.getEmail(),login.getPassword()));
             SecurityContextHolder.getContext().setAuthentication(authentication);
             User user = (User) authentication.getPrincipal();
             UserDto userDto = OwnMapper.INSTANCE.toUserDto(user);
-            return new LoginResponse(jwtUtils.generateJwtToken(userDto));
+            LoginResponse loginResponse =new LoginResponse(jwtUtils.generateJwtToken(userDto));
+            cookieService.addHttpOnlyCookie("jwt",loginResponse.getToken(),7*24*60*60,httpServletResponse);
+            System.out.println(httpServletResponse);
+            return loginResponse;
 
         } catch (BadCredentialsException e) {
             throw new InvalidAuthenticationException("Invalid username or password");
