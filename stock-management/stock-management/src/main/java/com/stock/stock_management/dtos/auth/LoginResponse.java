6f75df3c2b594fd5ec5d413351fbe61e4c472fe7
OMZ-IT4 <loulououssama2015@gmail.com>
2026-04-21 12:46:04 +0100

    token response

---
+++ stock-management/stock-management/src/main/java/com/stock/stock_management/dtos/auth/LoginResponse.java
@@ -0,0 +1,15 @@
+package com.stock.stock_management.dtos.auth;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class LoginResponse {
+    String token;
+}
