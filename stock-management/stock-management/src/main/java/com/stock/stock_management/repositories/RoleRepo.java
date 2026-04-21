10931146ff1f05285c3348b4bf8e2808cb43c23a
OMZ_IT <loulououssama2015@gmail.com>
2026-04-21 15:33:48 +0100

    added repository annotation

--- stock-management/stock-management/src/main/java/com/stock/stock_management/repositories/RoleRepo.java
+++ stock-management/stock-management/src/main/java/com/stock/stock_management/repositories/RoleRepo.java
@@ -2,9 +2,11 @@ package com.stock.stock_management.repositories;
 
 import com.stock.stock_management.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RoleRepo extends JpaRepository<Role,Long> {
     Optional<Role> findByName(String name);
 }
--- stock-management/stock-management/src/main/java/com/stock/stock_management/repositories/UserRepo.java
+++ stock-management/stock-management/src/main/java/com/stock/stock_management/repositories/UserRepo.java
@@ -1,10 +1,13 @@
 package com.stock.stock_management.repositories;
 
 import com.stock.stock_management.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface UserRepo {
+@Repository
+public interface UserRepo extends JpaRepository<User,Long> {
 
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
